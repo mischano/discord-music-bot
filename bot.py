b1b078346e39a3f10a5cecc9e75bbbb77d790b36
mischano <mansur.ischanov@gmail.com>
2023-01-22 13:30:20 -0800

    join finished. leave unfinished

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+from inspect import currentframe, getframeinfo
 
 load_dotenv()
 
@@ -19,5 +20,50 @@ def run():
     @client.command()
     async def play(ctx):
         await ctx.send("hello!")
+
+    @client.command()
+    async def join(ctx):
+        voice = ctx.author.voice
+        if voice is None:
+            await ctx.send("You are not connected to a voice channel.")
+            return
+
+        channel = voice.channel
+        try:
+            await channel.connect(timeout=.5, self_mute=False, self_deaf=True)
+        except discord.ClientException:
+            await ctx.send("Bot is already connected to a voice channel.")
+        except TimeoutError:
+            await ctx.send("Bot connection timed out.")
+
+    @client.command()
+    async def leave(ctx):
+        """
+        If the bot is not in a voice channel, raise exception   DONE
+        If the bot is by itself, then leave make the bot leave.
+        If leave is called by a member in the same voice chat, then bot leaves.
+        If leave is called by a member in a different voice chat:
+            1. If the bot by itself then bot leaves.
+            2. If the voice chat has other memebrs, then bot doesnt leave.
+        """
+        channel_obj = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        if channel_obj is None:
+            await ctx.send("Bot is not connected to a voice channel.")
+            return
+
+        channel_id = channel_obj.channel.id
+        current_channel = client.get_channel(channel_id)
+        if current_channel is None:
+            f_info = getframeinfo(currentframe())
+            await ctx.send("usage: channel not found. %s : %s" % (f_info.filename, f_info.lineno))
+
+        channel_members = len(current_channel.members)
+
+        if channel_members == 1:
+            vc = ctx.guild.voice_client
+            await vc.disconnect()
+            await ctx.send("Bye!")
+            return
+        
 
     client.run(TOKEN)
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import bot
 import spotify
 
 if __name__ == '__main__':
+    """
     token = spotify.get_token()
     artist = spotify.search_for_artist(token, "ACDC")
     artist_id = artist["id"]
     songs = spotify.get_songs_by_artist(token, artist_id)
-
+    
     for i, song in enumerate(songs):
         print(f"{i + 1}. {song['name']}")
+    """
 
     bot.run()
