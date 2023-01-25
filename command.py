c22240d9dce255f8b96c247cb0669cba6f5347ee
mischano <mansur.ischanov@gmail.com>
2023-01-24 22:46:19 -0800

    reimplemented join method in accordance with play method

--- command.py
+++ command.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from youtube_dl import YoutubeDL
 
 
 async def disconnect_bot(ctx, msg):
@@ -11,36 +12,60 @@ async def disconnect_bot(ctx, msg):
 
 class Command(commands.Cog):
     def __init__(self, client):
+        # Bot
         self.bot = client
         self.channel_name = None
         self.channel_id = None
         self.channel_member_num = None
 
     @commands.command()
-    async def test(self, ctx):
-        await ctx.send("hello!")
+    async def play(self, ctx, *args):
+        """
+        1. you are not connected to vc. Error
+        2. bot is not connected to vc.  Connect bot & play
+        3. you and bot (alone) are in different vc. Connect bot & play
+        4. you and bot (not alone) are in different vc. Error
+        5. you and bot are in the same vc. Play
+        """
+        if not await self.join(ctx):
+            print("doesn't play")
+        else:
+            print("plays")
 
     @commands.command()
     async def join(self, ctx):
-        voice = ctx.author.voice
-        if voice is None:
+        """
+        1. you are not connected to vc. Error
+        2. bot is not connected to vc.  Connect bot
+        3. you and bot (alone) are in different vc. Connect bot
+        4. you and bot (not alone) are in different vc. Error
+        5. you and bot are in the same vc. Error
+        """
+        caller = ctx.author.voice
+        if caller is None:
             await ctx.send("You are not connected to a voice channel.")
-            return
+            return False
 
-        channel = voice.channel
+        caller_channel = caller.channel
         try:
-            await channel.connect(timeout=.5, self_mute=False, self_deaf=True)
+            await caller_channel.connect(timeout=.5, self_mute=False, self_deaf=True)
+            return True
         except discord.ClientException:
-            if self.get_channel_info(ctx) is None:
-                await ctx.send("Bot is not connected to a voice channel.")
-                return
-
+            # if self.get_channel_info(ctx) is None:
+            #     await ctx.send("Bot is not connected to a voice channel.")
+            #     return
+            self.get_channel_info(ctx)
             if self.channel_member_num == 1:
                 await disconnect_bot(ctx, "")
-                await channel.connect(timeout=.5, self_mute=False, self_deaf=True)
-                return
+                await caller_channel.connect(timeout=.5, self_mute=False, self_deaf=True)
+                return True
             else:
-                await ctx.send("Bot is already connected to a voice channel.")
+                if caller_channel == self.channel_name:
+                    return True
+                else:
+                    await ctx.send("Bot is already connected to a voice channel.")
+                # if same channel return true, else error msg and return false
+                # return  # fail
         except TimeoutError:
             await ctx.send("Bot connection timed out.")
 
