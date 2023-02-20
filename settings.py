4cdf4cf596709e19d34a3848dc2615dd57747fb8
mischano <mansur.ischanov@gmail.com>
2023-02-20 11:02:49 -0800

    settings.py updated. Added bye message & playlist cleanup.

--- settings.py
+++ settings.py
@@ -1,19 +1,19 @@
 import asyncio
-import discord
+import playlist
+from stylizer import *
 from discord.ext import commands
 
 
 class Settings(commands.Cog):
     def __init__(self, client):
         self.client = client
-
         self.timeout = 300
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if not member.id == self.client.user.id:
             return
-        
+
         if before.channel is None:
             vc = after.channel.guild.voice_client
             elapsed_time = 0
@@ -23,9 +23,10 @@ class Settings(commands.Cog):
                 if vc.is_playing() and not vc.is_paused():
                     elapsed_time = 0
                 if elapsed_time == self.timeout:
-                    # channel = discord.utils.get(self.client.get_all_channels(), name=after)
+                    await member.guild.system_channel.send(f"Disconnected due to inactivity {emj_v_hand}")
                     await vc.disconnect()
                     vc.cleanup()
+                    playlist.clear_all()
                 if not vc.is_connected():
                     break   
 
